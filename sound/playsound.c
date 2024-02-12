77f7f333494ecd0209ffd13f08216a93d3679af7
ahkalama <116187665+ahkalama@users.noreply.github.com>
2024-02-12 22:07:50 +0300

    sdl2/sdl.h eklenecek playsound.c

--- sound/playsound.c
+++ sound/playsound.c
@@ -1,6 +1,33 @@
 #include <SDL2/SDL.h>
 
-int playsound()
+int main()
 {
-    playSound("sounds/door1.wav", SDL_MIX_MAXVOLUME / 2);
+    if (SDL_Init(SDL_INIT_AUDIO) < 0)
+    {
+        printf("SDL başlatılamadı: %s\n", SDL_GetError());
+        return 1;
+    }
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+    {
+        printf("Ses yüklenemedi: %s\n", Mix_GetError());
+        return 1;
+    }
+
+    Mix_Music *muzik = Mix_LoadMUS("sesdosyasi.mp3");
+    if (!muzik)
+    {
+        printf("Ses dosyası yüklenemedi: %s\n", Mix_GetError());
+        return 1;
+    }
+
+    Mix_PlayMusic(muzik, -1);
+
+    printf("Ses dosyasını çalmak için herhangi bir tuşa basınız...\n");
+    getchar();
+
+    Mix_FreeMusic(muzik);
+    Mix_CloseAudio();
+    SDL_Quit();
+
+    return 0;
 }
